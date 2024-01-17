c15ea8921e94ac38e473f4a8ed4f984751d09223
nikolamaric <maric.nikola022@gmail.com>
2024-01-17 14:47:12 +0100

    promijenio

--- player/apps/player/src/player.erl
+++ player/apps/player/src/player.erl
@@ -2,29 +2,23 @@
 -behaviour(gen_server).
 
 -export([start_link/0]).
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-        terminate/2, code_change/3]).
+-export([
+    init/1,
+    handle_call/3,
+    handle_cast/2,
+    handle_info/2,
+    terminate/2,
+    code_change/3
+]).
 -export([poruka/1, message_dealer/1, generate_card/0, calculate_sum/1]).
 
 -define(DEALER, 'dealer@localhost').
 
-calculate_sum(Cards) ->
-    calculate_sum(Cards, 0, 0).
-
-calculate_sum([], Sum, Aces) when Aces > 0, Sum + 10 + Aces =< 21 ->
-    % If there are Aces, adjust the sum to get the best poss
-    {Sum + 10 + Aces, soft};
-calculate_sum([], Sum, Aces) ->
-    {Sum + Aces, hard};
-calculate_sum([Card|Rest], Sum, Aces) when Card =:= 1 ->
-    % Handle Ace (value 1)
-    calculate_sum(Rest, Sum, Aces + 1);
-calculate_sum([Card|Rest], Sum, Aces) when Card >= 2, Card =< 10 ->
-    % Handle cards 2 through 10
-    calculate_sum(Rest, Sum + Card, Aces);
-calculate_sum([Card|Rest], Sum, Aces) when Card > 10 ->
-    % Handle face cards (Jack, Queen, King)
-    calculate_sum(Rest, Sum + 10, Aces).
+% hand --> karte koje sam povukao
+% stake --> ulog
+% money --> balans
+% error --> error poruka
+-record(state, {hand = [], stake = 0, money = 0, error = "none"}).
 
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
@@ -32,68 +26,6 @@ start_link() ->
 init([]) ->
     gen_server:cast(player, start),
     {ok, []}.
-
-handle_call(_Request, _From, State) ->
-    Reply = ok,
-    {reply, Reply, State}.
-
-handle_cast(start, _) ->
-    timer:sleep(1000),
-    io:format("~nDobrodosli u erlang-blackjack, upisite svoje ime: ~n"),
-    Name = io:get_line(""),
-    FormattedName = string:trim(Name),
-    io:format("~nUpisite svoj balans: ~n"),
-    MoneyStr = io:get_line(""),
-    {ok, [Money], _} = io_lib:fread("~d", MoneyStr),
-    io:format("~nUpisite svoj ulog: ~n"),
-    StakeStr = io:get_line(""),
-    {ok, [Stake], _} = io_lib:fread("~d", StakeStr),
-    net_kernel:start([list_to_atom(FormattedName), shortnames]),
-    io:format("Dobrodosli ~p!~nBalans: ~p~nUlof: ~p~n", [FormattedName, Money, Stake]),
-    net_adm:ping(?DEALER),
-    UpdatedState = {[], Stake, Money},
-    {noreply, UpdatedState};
-
-handle_cast(hit, State) when element(2,State) =< 0 ->
-    io:format("Morate staviti ulog.~n"),
-    {noreply, State};
-
-handle_cast(hit, State) when element(2,State) > element(3,State) ->
-    io:format("Nedovoljno novaca na računu, smanjite ulog.~n"),
-    io:format("Balans: ~p~n", [element(3,State)]),
-    {noreply, State};
-
-
-handle_cast(hit, State) ->
-    Card = generate_card(),
-    UpdatedState = {[Card | element(1,State)], element(2,State), element(3,State)},
-    io:format("Dobili ste kartu: ~p~n", [Card]),
-    io:format("Trenutne karte: ~p~n", [element(1,UpdatedState)]),
-    io:format("Suma: ~p~n", [calculate_sum(element(1,UpdatedState))]),
-    case {element(2, calculate_sum(element(1,UpdatedState))), element(1, calculate_sum(element(1,UpdatedState))) >= 21} of
-        {hard, true} ->
-            gen_server:cast(?MODULE, stand),
-            {noreply, UpdatedState};
-        _ ->
-            {noreply, UpdatedState}
-    end;
-
-handle_cast(stand, State) when element(1,State) == [] ->
-    io:format("Ne možete ostati ako nemate karata.~n"),
-    {noreply, State};
-
-handle_cast(stand, State) ->
-    io:format("Ostajete s kartama: ~p~n", [element(1,State)]),
-    io:format("Suma: ~p~n", [element(1,calculate_sum(element(1,State)))]),
-    io:format("Balans: ~p~n", [element(3, State) - element(2, State)]),
-    % Here you can implement further logic, such as sending the player's hand to the dealer.
-    {noreply, {[], element(2, State), element(3, State) - element(2, State)}};
-
-
-handle_cast(Msg, State) ->
-    io:format("player cast: ~p~n", [Msg]),
-    {noreply, State}.
-
 
 handle_info(Info, State) ->
     io:format("player info: ~p~n", [Info]),
@@ -112,8 +44,186 @@ poruka(Msg) ->
 message_dealer(Msg) ->
     rpc:call(?DEALER, dealer, poruka, [Msg]).
 
+% -----------------------------------------------------------------------------------------
+% ------------------------------------ CARD OPERATIONS ------------------------------------
+% -----------------------------------------------------------------------------------------
+
+calculate_sum(Cards) ->
+    calculate_sum(Cards, 0, 0).
+
+calculate_sum([], Sum, Aces) when Aces > 0, Sum + 10 + Aces =< 21 ->
+    {Sum + 10 + Aces, soft};
+calculate_sum([], Sum, Aces) ->
+    {Sum + Aces, hard};
+calculate_sum([Card | Rest], Sum, Aces) when Card =:= 1 ->
+    calculate_sum(Rest, Sum, Aces + 1);
+calculate_sum([Card | Rest], Sum, Aces) when Card >= 2, Card =< 10 ->
+    calculate_sum(Rest, Sum + Card, Aces);
+calculate_sum([Card | Rest], Sum, Aces) when Card > 10 ->
+    calculate_sum(Rest, Sum + 10, Aces).
+
+% -----------------------------------------------------------------------------------------
+% ----------------------------------- LOOP FOR INPUT --------------------------------------
+% -----------------------------------------------------------------------------------------
+
+% Helper function for the while loop
+loop_while_quit(State) ->
+    case io:get_line("INPUT:   ") of
+        "quit\n" ->
+            {noreply, State};
+        Input ->
+            NewState = element(2, handle_user_input(Input, State)),
+            case NewState#state.error of
+                "bust" ->
+                    io:format("BUST!~n"),
+                    handle_user_input("stand\n", NewState),
+                    loop_while_quit(NewState#state{
+                        hand = [],
+                        money = NewState#state.money - NewState#state.stake,
+                        error = "wait"
+                    });
+                "none" ->
+                    loop_while_quit(NewState);
+                "stake" ->
+                    io:format("Morate staviti ulog.~n"),
+                    loop_while_quit(NewState#state{error = "none"});
+                "money" ->
+                    io:format("Nedovoljno novaca na računu(~p), smanjite ulog(~p).~n", [
+                        NewState#state.money, NewState#state.stake
+                    ]),
+                    loop_while_quit(NewState#state{error = "none"});
+                "card" ->
+                    io:format("Ne možete ostati ako nemate karata.~n"),
+                    loop_while_quit(NewState#state{error = "none"});
+                "starterror" ->
+                    io:format("Igra je već započeta.~n"),
+                    loop_while_quit(NewState#state{error = "none"});
+                "hiterror" ->
+                    io:format("Morate započeti igru.~n"),
+                    loop_while_quit(NewState#state{error = "none"});
+                _ ->
+                    io:format("Error: ~p~n", [NewState#state.error]),
+                    loop_while_quit(NewState#state{error = "none"})
+            end
+    end.
+
+% Handle user input (hit, stand, or other actions)
+handle_user_input("hit\n", State) ->
+    handle_cast(hit, State);
+handle_user_input("stand\n", State) ->
+    handle_cast(stand, State);
+handle_user_input("start\n", State) ->
+    handle_cast(startgame, State);
+handle_user_input("stake\n", State) ->
+    io:format("Promijenite svoj ulog: "),
+    StakeStr = io:get_line(""),
+    {ok, [Stake], _} = io_lib:fread("~d", StakeStr),
+    {noreply, State#state{stake = Stake}};
+handle_user_input("help\n", State) ->
+    io:format(
+        "   HELP:~n\n"
+        "   --------------------------------\n"
+        "   start --- početak igre~n\n"
+        "   hit   --- vuci kartu~n\n"
+        "   stand --- dosta~n\n"
+        "   stake --- promjena uloga~n\n"
+        "   quit  --- izlaz~n\n"
+    ),
+    {noreply, State};
+handle_user_input(_, State) ->
+    io:format("Invalid input.~n"),
+    {noreply, State}.
+
+% -----------------------------------------------------------------------------------------
+% ------------------------------------- CALL HANDLERS -------------------------------------
+% -----------------------------------------------------------------------------------------
+
+handle_call(_Request, _From, State) ->
+    Reply = ok,
+    {reply, Reply, State}.
+
+% -----------------------------------------------------------------------------------------
+% ------------------------------------- CAST HANDLERS -------------------------------------
+% -----------------------------------------------------------------------------------------
+
+handle_cast(start, _) ->
+    timer:sleep(1000),
+    io:format("~nDobrodosli u erlang-blackjack, upisite svoje ime: ~n"),
+    Name = io:get_line(""),
+    FormattedName = string:trim(Name),
+    io:format("Upisite svoj balans: "),
+    MoneyStr = io:get_line(""),
+    {ok, [Money], _} = io_lib:fread("~d", MoneyStr),
+    io:format("Upisite svoj ulog: "),
+    StakeStr = io:get_line(""),
+    {ok, [Stake], _} = io_lib:fread("~d", StakeStr),
+    net_kernel:start([list_to_atom(FormattedName), shortnames]),
+    io:format("Dobrodosli ~p!~nBalans: ~p~nUlog: ~p~n", [FormattedName, Money, Stake]),
+    io:format("Za pomoc u igri napisite help!~n~n"),
+    net_adm:ping(?DEALER),
+    UpdatedState = #state{hand = [], stake = Stake, money = Money, error = "none"},
+    loop_while_quit(UpdatedState),
+    {noreply, UpdatedState};
+handle_cast(startgame, State) when State#state.stake =< 0 ->
+    {noreply, State#state{error = "stake"}};
+handle_cast(startgame, State) when State#state.stake > State#state.money ->
+    {noreply, State#state{error = "money"}};
+handle_cast(startgame, State) when length(State#state.hand) > 0 ->
+    {noreply, State#state{error = "starterror"}};
+handle_cast(startgame, State) ->
+    Hand = [generate_card(), generate_card()],
+    io:format("Dobili ste karte: ~p~n", [Hand]),
+    io:format("Suma: ~p~n", [calculate_sum(Hand)]),
+    {noreply, State#state{hand=Hand}};
+handle_cast(hit, State) when length(State#state.hand) == 0 ->
+    UpdatedState = State#state{error = "hiterror"},
+    {noreply, UpdatedState};
+handle_cast(hit, State) ->
+    Card = generate_card(),
+    UpdatedState = State#state{hand = [Card | State#state.hand]},
+    io:format("Dobili ste kartu: ~p~n", [Card]),
+    io:format("Trenutne karte: ~p~n", [UpdatedState#state.hand]),
+    io:format("Suma: ~p~n", [calculate_sum(UpdatedState#state.hand)]),
+    case
+        {
+            element(2, calculate_sum(UpdatedState#state.hand)),
+            element(1, calculate_sum(UpdatedState#state.hand)) > 21
+        }
+    of
+        {hard, true} ->
+            {noreply, State#state{hand = [Card | State#state.hand], error = "bust"}};
+        _ ->
+            {noreply, UpdatedState}
+    end;
+handle_cast(stand, State) when State#state.hand == [] ->
+    UpdatedState = State#state{error = "card"},
+    {noreply, UpdatedState};
+handle_cast(stand, State) ->
+    io:format("Ostajete s kartama: ~p~n", [State#state.hand]),
+    io:format("Suma: ~p~n", [element(1, calculate_sum(State#state.hand))]),
+    io:format("Balans: ~p~n", [State#state.money - State#state.stake]),
+    % Here you can implement further logic, such as sending the player's hand to the dealer.
+    UpdatedState = #state{
+        hand = [],
+        stake = State#state.stake,
+        money = State#state.money - State#state.stake,
+        error = "none"
+    },
+    {noreply, UpdatedState};
+handle_cast(Msg, State) ->
+    io:format("player cast: ~p~n", [Msg]),
+    {noreply, State}.
+
+% -----------------------------------------------------------------------------------------
+% ------------------------------- FUNKCIJE KOJE ZOVE PLAYER -------------------------------
+% -----------------------------------------------------------------------------------------
+
 % koristi se za testiranje.
 generate_card() ->
-    Numbers = lists:flatten(lists:duplicate(4, lists:seq(1, 9) ++ [10, 10, 10, 10])),
+    Numbers = lists:flatten(lists:duplicate(4, lists:seq(1, 13))),
     N = rand:uniform(length(Numbers)),
     lists:nth(N, Numbers).
+
+% -----------------------------------------------------------------------------------------
+% ------------------------------- FUNKCIJE KOJE ZOVE DEALER -------------------------------
+% -----------------------------------------------------------------------------------------
